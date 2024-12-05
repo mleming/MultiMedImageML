1eb1a08a3406b50e76608862ad6f164a99f49c03
mleming <matthew.j.leming@gmail.com>
2024-12-05 16:56:58 -0500

    Updated unit tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -59,6 +59,8 @@ class TestSimple(unittest.TestCase):
 		nifti_file,json_file = compile_dicom(im1,cache=True)
 		self.assertEqual(len_glob, len(glob.glob(os.path.join(im1,'*'))))
 		nifti_file2,json_file2 = compile_dicom(im1,cache=False)
+		#print(nifti_file)
+		#print(nifti_file2)
 		self.assertTrue(nifti_file2 != nifti_file)
 		self.assertTrue(json_file2 != json_file)
 		if os.path.splitext(json_file)[1] == ".json":
@@ -69,6 +71,7 @@ class TestSimple(unittest.TestCase):
 			os.remove(json_file2)
 		if os.path.splitext(nifti_file2)[1] == ".gz":
 			os.remove(nifti_file2)
+
 	def test_single_im_load(self):
 		im = ImageRecord(im1,X_dim=(24,48,32),cache=False)
 		img = im.get_X()
@@ -81,11 +84,11 @@ class TestSimple(unittest.TestCase):
 		self.assertEqual(img.shape[0], 33)
 		self.assertEqual(img.shape[1], 16)
 		self.assertEqual(img.shape[2], 3)
-		self.assertTrue(im.cached_record is not None)
-		self.assertTrue(os.path.isfile(im.cached_record))
-		self.assertEqual(os.path.splitext(im.cached_record)[1], ".npy")
-		if os.path.isfile(im.cached_record):
-			os.remove(im.cached_record)
+		self.assertTrue(im.npy_file is not None)
+		self.assertTrue(os.path.isfile(im.npy_file))
+		self.assertEqual(os.path.splitext(im.npy_file)[1], ".npy")
+		if os.path.isfile(im.npy_file):
+			os.remove(im.npy_file)
 	def test_single_nifti_load(self):
 		im = ImageRecord(nifti_im,X_dim=(4,5,6),cache=True)
 		img = im.get_X()
@@ -172,7 +175,7 @@ class TestSimple(unittest.TestCase):
 				fpath = os.path.join(root,name)
 				if rstr in fpath:
 					self.assertTrue(fpath in df.index)
-		
+	
 	def test_pandas_2(self):
 		medim_loader = MedImageLoader(imfolder1,imfolder2,
 			X_dim=(48,32,24),
@@ -217,6 +220,7 @@ class TestSimple(unittest.TestCase):
 			self.assertEqual(imsize[1], 48)
 			self.assertEqual(imsize[2], 32)
 			self.assertEqual(imsize[3], 24)
+
 	def test_grouping(self):
 		medim_loader = MedImageLoader(imfolder1,imfolder2,
 			X_dim=(48,32,24),
@@ -246,6 +250,7 @@ class TestSimple(unittest.TestCase):
 		C = patient.get_C()
 		C_dud = patient.get_C_dud()
 		Y = patient.get_Y()
+
 	def test_more(self):
 		medim_loader = MedImageLoader(imfolder1)
 		for image in medim_loader:
@@ -253,16 +258,16 @@ class TestSimple(unittest.TestCase):
 		medim_loader = MedImageLoader(imfolder2,augment=True)
 		for image in medim_loader:
 			continue
+
 	def test_model(self):
-		model = MultiInputModule(Y_dim=(32,32),C_dim=(32,32))
+		model = MultiInputModule()
 		#medim_loader = MedImageLoader(imfolder1,imfolder2,
 		#	cache=True)
 		#for image,label in medim_loader: continue
 		medim_loader = MedImageLoader(imfolder1,imfolder2,
 			return_obj=True,
 			cache=True,dtype="torch",
-			Y_dim = (32,32),
-			C_dim = (32,32),batch_by_pid=True)
+			batch_by_pid=True)
 		optimizer = torch.optim.Adam(
 			model.classifier_parameters(),
 			betas = (0.5,0.999),
@@ -272,13 +277,17 @@ class TestSimple(unittest.TestCase):
 		
 		for p in medim_loader:
 			optimizer.zero_grad()
-			y_pred,y_reg = model(p)
+			y_pred = model(p)
 			loss = loss_function(p.get_Y(),y_pred)
 			loss.backward()
 			optimizer.step()
 			break
+
 	def test_trainer(self):
-		model = MultiInputModule(Y_dim = (17,4),C_dim=(13,11))
+		model = MultiInputModule(label=["MRAcquisitionType",
+					"ImageOrientationPatientDICOM"],
+			confounds=["Slice Thickness","Repetition Time"],
+			encode_age=False)
 		medim_loader = MedImageLoader(imfolder1,imfolder2,
 			cache=True,
 			label=["MRAcquisitionType",
@@ -286,16 +295,18 @@ class TestSimple(unittest.TestCase):
 			confounds=["Slice Thickness","Repetition Time"],
 			return_obj = True,
 			dtype="torch",
-			batch_size=14,Y_dim=(17,4),C_dim=(13,11),
+			batch_size=14,
 			batch_by_pid=True)
-		trainer = MultiInputTrainer(model,batch_size=2)
+		trainer = MultiInputTrainer(model,medim_loader,batch_size=2)
 		for i in range(3):
 			#print(f"Epoch {i}")
 			for p in medim_loader:
-				trainer.loop(p,dataloader=medim_loader)
-		
+				trainer.loop(p)
+
 	def test_cache2(self):
-		model = MultiInputModule(Y_dim = (13,14),C_dim=(19,21))
+		model = MultiInputModule(label=["MRAcquisitionType","Manufacturer"],
+			confounds=["Slice Thickness","Repetition Time"],
+			encode_age=False)
 		medim_loader = MedImageLoader(imfolder1,imfolder2,
 			cache=False,
 			label=["MRAcquisitionType",
@@ -303,17 +314,18 @@ class TestSimple(unittest.TestCase):
 			confounds=["Slice Thickness","Repetition Time"],
 			return_obj = True,
 			dtype="torch",
-			Y_dim = (13,14),
-			C_dim=(19,21),
 			batch_by_pid=True)
-		trainer = MultiInputTrainer(model,batch_size=2)
+		trainer = MultiInputTrainer(model,medim_loader,batch_size=2)
 		for i in range(3):
 			#print(f"Epoch {i}")
 			for p in medim_loader:
-				trainer.loop(p,dataloader=medim_loader)	
+				trainer.loop(p)	
 
 	def test_dyn_input(self):
-		model = MultiInputModule(Y_dim = (13,14),C_dim=(19,21))
+		model = MultiInputModule(
+				label=["MRAcquisitionType","Manufacturer"],
+				confounds=["Slice Thickness","Repetition Time"],
+				n_stat_inputs = 1)
 		medim_loader = MedImageLoader(imfolder1,imfolder2,
 			cache=False,
 			label=["MRAcquisitionType",
@@ -321,16 +333,17 @@ class TestSimple(unittest.TestCase):
 			confounds=["Slice Thickness","Repetition Time"],
 			return_obj = True,
 			dtype="torch",
-			batch_size=14,Y_dim = (13,14),C_dim=(19,21),
-			static_inputs = ["User Data 7","Slice Thickness"],
+			batch_size=14,
+			static_inputs = ["Pixel Representation"],
 			batch_by_pid=True)
-		trainer = MultiInputTrainer(model,batch_size=2)
+		trainer = MultiInputTrainer(model,medim_loader,batch_size=2)
 		for i in range(3):
 			for p in medim_loader:
-				trainer.loop(p,dataloader=medim_loader)
+				trainer.loop(p)
 
 	def test_cache3(self):
-		model = MultiInputModule((11,17),C_dim=(23,5))
+		model = MultiInputModule(label=["MRAcquisitionType","Manufacturer"],
+			confounds=["Slice Thickness","Repetition Time"],encode_age=False)
 		medim_loader = MedImageLoader(imfolder1,imfolder2,
 			cache=True,
 			label=["MRAcquisitionType",
@@ -338,40 +351,88 @@ class TestSimple(unittest.TestCase):
 			confounds=["Slice Thickness","Repetition Time"],
 			return_obj = True,
 			dtype="torch",
-			batch_size=14,Y_dim=(11,17),C_dim=(23,5),
+			batch_size=14,
 			batch_by_pid=True)
-		trainer = MultiInputTrainer(model,
+		trainer = MultiInputTrainer(model,medim_loader,
 					batch_size=2,
-					loss_image_dir = 'test_folder/loss_images',
+					out_record_folder = 'test_folder',
 					checkpoint_dir = 'test_folder/checkpoints',
 					name = 'unit_test',
 					verbose = False,
 					save_latest_freq = 1)
 		for i in range(3):
 			for p in medim_loader:
-				trainer.loop(p,dataloader=medim_loader)	
+				trainer.loop(p)
+
 	def test_weight_download(self):
+		return
 		model = MultiInputModule(weights="unit_test")
-	def test_init(self):
-		model = MultiInputModule((1,17),C_dim=(23,5))
+
+	def test_model_struct(self):
+		model = MultiInputModule(label=["MRAcquisitionType"],
+			confounds=["Slice Thickness","Repetition Time"])
 		medim_loader = MedImageLoader(imfolder1,imfolder2,
 			cache=True,
 			label=["MRAcquisitionType"],
 			confounds=["Slice Thickness","Repetition Time"],
 			return_obj = True,
 			dtype="torch",
-			batch_size=14,Y_dim=(1,17),C_dim=(23,5),
+			batch_size=14,
 			batch_by_pid=True)
 		tester = MultiInputTester(medim_loader.database,model,
 			out_record_folder='test_folder',
-			checkpoint_dir='test_folder/checkpoints',
-			database_key="Manufacturer")
+			checkpoint_dir='test_folder/checkpoints')
+		self.assertTrue("MRAcquisitionType" in model.classifiers)
+		self.assertTrue("Slice Thickness" in model.regressors)
+		self.assertTrue("Repetition Time" in model.regressors)
+		sd = model.state_dict()
+		self.assertTrue("regressor.Repetition Time" in sd)
+		self.assertTrue("classifier.MRAcquisitionType" in sd)
+		self.assertTrue("regressor.Slice Thickness" in sd)
+
+	def test_init(self):
+		model = MultiInputModule(label=["MRAcquisitionType"],
+			confounds=["Slice Thickness","Repetition Time"])
+		medim_loader = MedImageLoader(imfolder1,imfolder2,
+			cache=True,
+			label=["MRAcquisitionType"],
+			confounds=["Slice Thickness","Repetition Time"],
+			return_obj = True,
+			dtype="torch",
+			batch_size=14,
+			batch_by_pid=True)
+		tester = MultiInputTester(medim_loader.database,model,
+			out_record_folder='test_folder',
+			checkpoint_dir='test_folder/checkpoints')
 		for pr in medim_loader:
 			tester.loop(pr)
-		tester.stats_record.record()
+		tester.record()
 		tester.read_json()
-		tester.plot()
-		tester.test_grad_cam(pr)
+		#auc = tester.auc()
+		auc = tester.acc(database_key = "Manufacturer")
+		tester.plot(database_key = "Manufacturer")
+		tester.plot(database_key = "Manufacturer",
+			same_pids_across_groups=False,
+			opt = "name_num")
+		tester.grad_cam(pr)
+	def test_train(self):
+		model = MultiInputModule(label = ["MRAcquisitionType"],
+			confounds=["Slice Thickness","Repetition Time"])
+		medim_loader = MedImageLoader(imfolder1,imfolder2,
+			cache=True,
+			label=["MRAcquisitionType"],
+			confounds=["Slice Thickness","Repetition Time"],
+			return_obj = True,
+			dtype="torch",
+			batch_size=14,
+			batch_by_pid=True)
+		trainer = MultiInputTrainer(model,
+			dataloader = medim_loader,
+			out_record_folder='test_folder',
+			checkpoint_dir='test_folder/checkpoints')
+		for pr in medim_loader:
+			trainer.loop(pr)
+		
 if __name__ == "__main__":
 	#clear_files()
 	# Runs the tests twice, once with cached files and once without
